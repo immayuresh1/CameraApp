fa890289ff641f9a4de200d14aa51355a7f4b118
Mayuresh Musale <53152670+immayuresh1@users.noreply.github.com>
2021-02-02 23:37:08 -0800

    Update App.js

--- App.js
+++ App.js
@@ -74,7 +74,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     flexDirection: 'column',
     backgroundColor: '#0a79bf',
   },
